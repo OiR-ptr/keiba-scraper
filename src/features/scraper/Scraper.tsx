07c2f59f8e198bc1097fab2f61fb593bbde57066
OiR-ptr <myr.ryusuke@gmail.com>
2022-01-31 00:19:35 +0900

    :construction: add fetch page

--- src/features/scraper/Scraper.tsx
+++ src/features/scraper/Scraper.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Button, TextField } from '@mui/material';
+import GetAppIcon from '@mui/icons-material/GetApp';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 
 export function Scraper() {
+  const [raceUrl, setRaceUrl] = React.useState('');
   const dispatch = useAppDispatch();
 
+  const fetchFromNetKeiba = (url : string) => {
+    console.log(url);
+  };
+
   return (
-    <Typography>This is body</Typography>
+    <div>
+      <TextField fullWidth id="race_id" label="race_id" variant="outlined" value={raceUrl} 
+        onChange={(e) => setRaceUrl(e.target.value)}
+        placeholder='https://race.netkeiba.com/race/shutuba.html?race_id=XXXXXX' />
+      <Button variant="outlined" endIcon={<GetAppIcon />} 
+        onClick={(e) => fetchFromNetKeiba(raceUrl)}>Fetch</Button>
+    </div>
   );
 }
