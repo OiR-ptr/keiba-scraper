d10407d39f0e582c4ed5edd9595defcb2ebd1d36
OiR-ptr <myr.ryusuke@gmail.com>
2022-03-13 11:38:36 +0900

    :sparkles: refactor API codes

--- src/features/scraper/scraperAPI.ts
+++ src/features/scraper/scraperAPI.ts
@@ -1,3 +1,16 @@
+type Query = { query: string };
+
+function toGraphQL(query: Query) {
+  return fetch('https://vocal-bug-76.hasura.app/v1/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-hasura-admin-secret': `${process.env.REACT_APP_HASURA_API_KEY}`,
+    },
+    body: JSON.stringify(query)
+  });
+}
+
 export function fetchRace() {
   const query = {
     query: 
@@ -14,15 +27,17 @@ export function fetchRace() {
           comment
         }
       }
-    }`
+    }`,
   };
 
-  return fetch('https://vocal-bug-76.hasura.app/v1/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-hasura-admin-secret': `${process.env.REACT_APP_HASURA_API_KEY}`,
-    },
-    body: JSON.stringify(query)
-  });
+  return toGraphQL(query);
+}
+
+export function registerRace() {
+  const query = {
+    query: 
+    ``,
+  };
+
+  return toGraphQL(query);
 }
